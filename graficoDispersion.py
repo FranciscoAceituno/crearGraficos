80fb48d77f1b861a5cc04dc2f7b041539d3cbdd6
faceitunof@gmail.com <faceitunof@gmail.com>
2024-04-19 00:20:20 -0400

    cambios en los graficos

--- graficoDispersion.py
+++ graficoDispersion.py
@@ -27,33 +27,51 @@ class GraficoDispersion:
         # creamos una nueva columna para almacenar el total de la población de cada código postal
         sales_data.insert(3,'totalPoblacion',colPoblacionTotal,True)
 
+        
         # Presentamos en pantalla los resultados del análisis
-        print('Total población usa transporte público --> ', round(sales_data['public_transportation_population'].sum(),2))
-        print('total de la población --> ', round(sales_data['totalPoblacion'].sum(),2))
-        pctRealUsoTransporte = round((sales_data['public_transportation_population'].sum() / sales_data['totalPoblacion'].sum())*100,2 )
-        print('promedio de uso de transporte público --> ', pctRealUsoTransporte)
+        print("Resumen Potenciales clientes")
+        print("**"*35)
+
+        totalPoblacionUsoTransporte = round(sales_data['public_transportation_population'].sum(),2)
+        print(f'\nTotal población usa transporte público --> {totalPoblacionUsoTransporte}' )
+
+        totalPoblacion = round(sales_data['totalPoblacion'].sum(),0)
+        print(f'Total de la población --> {totalPoblacion}' )
+
+        posiblesClientes = sales_data[sales_data['public_transportation_pct'] > 40]
+
+        
+        pctPosiblesClientes = round((posiblesClientes['public_transportation_population'].sum() / posiblesClientes['totalPoblacion'].sum())*100,2 )
+        print(f'\nPorcentaje de potenciales clientes {pctPosiblesClientes}% *')
+
+        cantPosiblesClientes = posiblesClientes['public_transportation_population'].sum()
+        print(f'Cantidad de potenciales clientes {cantPosiblesClientes} *')
+        print('\n\n*Este porcentaje y número se obtuvo considerando que dentro del\ncódigo postal el uso del transporte sea superior al 40%')
+        
+
+        input('\nPresione una tecla para ver el gráfico...')
 
         # Paso 4: Crear un diagrama de dispersión
         # Graficar el primer diagrama de dispersión en el primer subplot (ax1)
         fig, (ax1,ax2) = plt.subplots(1, 2, figsize=(12, 6))  # 1 fila, 2 columnas
         ax1.scatter(sales_data['totalPoblacion'],sales_data['public_transportation_pct'], alpha=0.5, color='blue')
-        ax1.set_title('Relación entre Uso de Transporte Público y Ventas Estimadas por Código Postal')
+        ax1.set_title('Uso de Transporte Público vs Porcentaje de uso sin filtrar')
         ax1.set_xlabel('Cantidad de Habitantes')
         ax1.set_ylabel('Porcentaje de Uso de Transporte Público')
+        ax1.xaxis.set_major_locator(plt.MultipleLocator(5000))
+        ax1.yaxis.set_major_locator(plt.MultipleLocator(5)) 
         ax1.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x)}%'))
 
         # Graficar el segundo diagrama de dispersión en el segundo subplot (ax2)
+        ax2.scatter(posiblesClientes['totalPoblacion'],posiblesClientes['public_transportation_pct'], alpha=0.5, color='red')
+        ax2.set_title(f'Gráfico que presenta los Potenciales clientes\n corresponde al {pctPosiblesClientes} % del total de la población')
+        ax2.set_xlabel('Cantidad de Habitantes')
+        ax2.set_ylabel('Porcentaje de Uso de Transporte Público')
+        ax2.xaxis.set_major_locator(plt.MultipleLocator(5000))
+        ax2.set_ylim(0, 110)
+        ax2.yaxis.set_major_locator(plt.MultipleLocator(10)) 
+        ax2.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x)}%'))
 
-        ax2.scatter(pctRealUsoTransporte, 0, color='red')
-        ax2.annotate(f'{pctRealUsoTransporte}%',(pctRealUsoTransporte,0), textcoords="offset points", xytext=(0,10), ha='center')
-        ax2.set_xlim(0, 100)
-        ax2.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x)}%'))
-        ax2.set_title('Gráfico de Análisis de Posibles Ventas')
-        ax2.set_xlabel('Porcentaje posibles ventas')
-        ax2.get_yaxis().set_visible(False)
-        ax2.set_aspect(2.0) 
-        ax2.set_xlim(0, 15)  # Ampliar los límites en el eje x
-        ax2.set_ylim(-1, 8) 
 
         # Ajustar automáticamente el diseño de los subplots
         plt.tight_layout()
@@ -62,5 +80,6 @@ class GraficoDispersion:
         plt.show()
 
         # Paso 5: Exportar los datos a un archivo Excel
+        posiblesClientes = posiblesClientes.sort_values(by='zip_code', ascending=True)
         output_file_path = "Resultado Analisis posibles ventas.xlsx" 
-        sales_data.to_excel(output_file_path, index=False)
+        posiblesClientes.to_excel(output_file_path, index=False)
--- histograma.py
+++ histograma.py
@@ -12,25 +12,33 @@ class Histograma:
     
     def createHistogram(self):
         dfGrafico = self.df.drop('zip_code', axis=1)
-        dfGrafico = dfGrafico[dfGrafico['public_transportation_pct'] > 10]
-        pctVsPoblacion = dfGrafico.groupby('public_transportation_pct').sum().reset_index()
+        dfGrafico = dfGrafico[dfGrafico['public_transportation_pct'] > 0]
+        dfGraficoBajoUso = dfGrafico[dfGrafico['public_transportation_pct'] <= 10]
+        dfGraficoAltoUso = dfGrafico[dfGrafico['public_transportation_pct'] > 10]
+        pctVsPoblacionBajo = dfGraficoBajoUso.groupby('public_transportation_pct').sum().reset_index()
+        pctVsPoblacionAlto = dfGraficoAltoUso.groupby('public_transportation_pct').sum().reset_index()
 
         # pctVsPoblacion.to_excel("salidaPCT.xlsx", index=0)
 
-        # print(pctVsPoblacion)
-        sns.histplot(data=pctVsPoblacion, x="public_transportation_pct", weights="public_transportation_population",bins=100, kde=False)
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16,8), sharex=True) # 1 fila, 2 columnas
+       # Graficar el primer histograma en el primer subplot (ax1) con color azul
+        ax1.hist(data = pctVsPoblacionBajo, x="public_transportation_pct", weights="public_transportation_population", alpha=0.5, color='blue',edgecolor='black', linewidth=1.2, label='Población con uso de 0 al 10 %')
+        ax1.set_xlim(0, 50)
+        ax1.xaxis.set_major_locator(plt.MultipleLocator(10)) 
+        ax1.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x)}%'))
+        ax1.legend()
 
-        plt.xlabel('Porcentaje')
-        plt.ylabel('')
-        plt.title('Histograma de Porcentajes VS Población')
-        plt.gca().xaxis.set_major_formatter(mtick.PercentFormatter())
+        # Graficar el segundo histograma en el segundo subplot (ax2) con color naranja
+        ax2.hist(data= pctVsPoblacionAlto, x="public_transportation_pct", weights="public_transportation_population", alpha=0.5, color='red',edgecolor='black', linewidth=1.2, label='Población con uso sobre el 10 %')
+        ax2.set_xlim(0, 100)
+        ax2.xaxis.set_major_locator(plt.MultipleLocator(10)) 
+        ax2.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x)}%'))
+        ax2.legend()
 
+        fig.suptitle('Comparación de Histogramas', fontsize=14)
+
+        # Ajustar el diseño de los subplots para evitar superposiciones
+        plt.tight_layout()
+
+        # Mostrar el gráfico
         plt.show()
-
-
-
-# df = pd.read_csv('public_transportation_statistics_by_zip_code.csv')
-# df.dropna(inplace=True)
-# df = df[df['public_transportation_pct'] > 0]
-# print("DataFrame original:")
-
